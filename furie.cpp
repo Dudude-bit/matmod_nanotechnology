5f1ca3472e5ec5b4809cd5bc5ffd6c0acfed2c31
kiinyakin <kiinyakin@mts.ru>
2022-10-17 23:49:41 +0300

    f

---
+++ furie.cpp
@@ -0,0 +1,52 @@
+#include "math.h"
+#include "iostream"
+
+using namespace std;
+
+
+float F(float x) {
+    return (10 - x) * x * exp(-pow(x, 0.5));
+}
+
+long double g(float i, float k, float N) {
+    return sqrt(2 / N) * sin((M_PI / N) * i * k);
+}
+
+int main() {
+    int N = 8;
+    float a = 0, b = 10;
+    long double A[N][N], A_T[N][N], f[N], x[N], Y[N], E[N], D[N];
+    float h = (b - a) / N;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++) {
+            A[i][j] = g(i, j, N);
+            A_T[j][i] = A[i][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        x[i] = i * h;
+    }
+
+    for (int i = 0; i < N; i++) {
+        f[i] = F(x[i]);
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++){
+            Y[i] += A[i][j] * f[j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++){
+            E[i] += A_T[i][j] * Y[j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        D[i] = E[i] - f[i];
+    }
+
+
+}
