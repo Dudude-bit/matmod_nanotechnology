766d0bf9e42d0f4afab67906de552507b1e9dd1b
kiinyakin <kiinyakin@mts.ru>
2022-11-07 02:02:39 +0300

    f

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include "iostream"
+#include "fstream"
 #include "cmath"
 
 using namespace std;
-const double PI = 3.141592653589793;
 
+const long double E_0 = 0.0000000001;
 const double t = 0.01;
-
 const unsigned int N = 101;
 const double h = 1. / (N - 1);
+
+void writeToFile(double arr[N], double s, const string& fileName) {
+    ofstream file(fileName);
+
+    for (int i = 0; i < N; i++) {
+        file << arr[i] << endl;
+    }
+    file << "Number of iterations: " << s;
+}
 
 void furier(double A[N], double B[N]) {
     double sum;
@@ -16,47 +25,60 @@ void furier(double A[N], double B[N]) {
     for (k = 0; k < N; k++) {
         sum = 0.;
         for (j = 0; j < N; j++) {
-            sum += A[j] * sin((j * PI * k) / (N - 1.));
+            sum += A[j] * sin((j * M_PI * k) / (N - 1.));
         }
         B[k] = sqrt(2. / (N - 1.)) * sum;
     }
 }
 
-double solver(double C_I, double P_K_I, double LAMBDA_I) {
-    return (C_I + t * P_K_I) / (1. + t * LAMBDA_I);
+double P_f(double x, double time) {
+    return (x*x*x - 4*x*x + 3*x)*exp(1.-time);
 }
 
+void solver(const double C[N], double CSP1[N], const double lambda[N], const double P_K[N]) {
+    for (int i = 1; i < N; i++) {
+        CSP1[i] = (C[i] + t * P_K[i]) / (1. + t * lambda[i]);
+    }
+}
 
-double P_f(double x) {
-    return x * (1. - x);
+double MaxByAbsV(double arr[N]) {
+    double max_v = abs(arr[0]);
+
+    for (int i = 1; i < N; i++) {
+        max_v = max(max_v, abs(arr[i]));
+    }
+
+    return max_v;
 }
 
 int main() {
-    double W[N], C[N], P[N], P_K[N], CSP1[N], WSP1[N], lambda[N];
+    long double E;
+    double P[N], P_K[N], W[N], lambda[N], C[N], CSP1[N], WSP1[N];
+    int s = 0;
 
     for (int i = 0; i < N; i++) {
         W[i] = 0.;
-        P[i] = P_f(i / ((double)N - 1.));
-        lambda[i] = (4. / pow(h, 2.)) * pow(sin((PI * i) / (2. * ((double)N - 1.))), 2);
+        lambda[i] = (4. / (h*h)) * pow(sin((M_PI * i) * h / 2.), 2.);
     }
 
-    furier(P, P_K);
+    do {
+        for (int i = 0; i < N; i++) {
+            P[i] = P_f(i * h, s * t);
+        }
+        furier(P, P_K);
 
-    for (int k = 1; k < N; k++) {
         furier(W, C);
-    }
 
-    for (int i = 1; i < N; i++) {
-        CSP1[i] = solver(C[i], P_K[i], lambda[i]);
-    }
+        solver(C, CSP1, lambda, P_K);
 
-    furier(CSP1, WSP1);
+        furier(CSP1, WSP1);
 
-    for (int i = 1; i < N; i++) {
-        W[i] = WSP1[i];
-    }
+        swap(W, WSP1);
 
-    for (int i = 0; i < N; i++) {
-        cout << W[i] << endl;
-    }
+        s++;
+
+        E = (abs((MaxByAbsV(WSP1) - MaxByAbsV(W))) * 2) / (MaxByAbsV(WSP1) + MaxByAbsV(W));
+    } while (E_0 < E);
+
+    writeToFile(W, s, "../file.txt");
 }
