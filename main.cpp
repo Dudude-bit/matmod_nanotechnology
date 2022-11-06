532e75c4f5821db02f5af6f7e0f42859f86ee5ff
kiinyakin <kiinyakin@mts.ru>
2022-11-06 20:45:03 +0300

    solve with do while

--- main.cpp
+++ main.cpp
@@ -2,38 +2,42 @@
 #include "cmath"
 
 using namespace std;
+const double PI = 3.141592653589793;
 
-const long double t = 1 / 20;
+const double t = 0.01;
 
 const unsigned int N = 101;
+const double h = 1. / (N - 1);
 
-void furier(long double A[N], long double B[N]) {
-    long double sum;
+void furier(double A[N], double B[N]) {
+    double sum;
     int k, j;
 
     for (k = 0; k < N; k++) {
         sum = 0.;
         for (j = 0; j < N; j++) {
-            sum += A[j] * sin((j * M_PI * k) / (N - 1));
+            sum += A[j] * sin((j * PI * k) / (N - 1.));
         }
         B[k] = sqrt(2. / (N - 1.)) * sum;
     }
 }
 
+double solver(double C_I, double P_K_I, double LAMBDA_I) {
+    return (C_I + t * P_K_I) / (1. + t * LAMBDA_I);
+}
 
-long double P_f(long double x) {
+
+double P_f(double x) {
     return x * (1. - x);
 }
 
 int main() {
-//    int S = 0;
-    long double W[N], C[N], P[N], P_K[N], CSP1[N], WSP1[N], lambda[N];
+    double W[N], C[N], P[N], P_K[N], CSP1[N], WSP1[N], lambda[N];
 
     for (int i = 0; i < N; i++) {
         W[i] = 0.;
-        C[i] = 0.;
-        P[i] = P_f((i / ((long double) N - 1.)));
-        lambda[i] = (4. / pow((long double) N - 1., 2.)) * sin((M_PI * (long double) N) / (2. * ((long double) N - 1.)));
+        P[i] = P_f(i / ((double)N - 1.));
+        lambda[i] = (4. / pow(h, 2.)) * pow(sin((PI * i) / (2. * ((double)N - 1.))), 2);
     }
 
     furier(P, P_K);
@@ -43,7 +47,7 @@ int main() {
     }
 
     for (int i = 1; i < N; i++) {
-        CSP1[i] = (C[i] + t * P_K[i]) / (1. + t * lambda[i]);
+        CSP1[i] = solver(C[i], P_K[i], lambda[i]);
     }
 
     furier(CSP1, WSP1);
